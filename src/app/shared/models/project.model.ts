9208e2dd053322b010c3df4d647cd85468dfe22e
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-04-19 17:44:07 +0200

    feat(project): update project models

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -2,11 +2,31 @@ import { EntityState } from '@ngrx/entity';
 
 export interface Project {
   id: string;
-  title: string;
-  owner: string;
-  techno: string;
-  description: string;
+  name: string;
+  lastVersion: number;
+  language: ProjectLanguage;
+  status: ProjectStatus;
+  globalVisibility: ProjectVisibility;
+  creatorId: string;
+  groupId: string;
   createdAt: Date;
+}
+
+export enum ProjectLanguage {
+  ANGULAR = 'ANGULAR',
+  REACT = 'REACT'
+}
+
+export enum ProjectStatus {
+  INITIALISING = 'INITIALISING',
+  INACTIVE = 'INACTIVE',
+  RUNNING = 'RUNNING'
+}
+
+export enum ProjectVisibility {
+  PRIVATE,
+  PUBLIC,
+  GUEST
 }
 
 export type ProjectState = EntityState<Project>;
