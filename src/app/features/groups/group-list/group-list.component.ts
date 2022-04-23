82dfd49615f5bdf6ff40dc5432d4276ef7e21e9c
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-04-23 12:41:40 +0200

    feat(cc-61): prepare dispatching retrieve of groups

--- src/app/features/groups/group-list/group-list.component.ts
+++ src/app/features/groups/group-list/group-list.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { groupsNavigation } from '../groups-routing.module';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../../../core/core.state';
+import { Observable } from 'rxjs';
+import { Group } from '../../../shared/models/group.model';
+import { actionGroupsRetrieveAll } from '../store/groups.actions';
+import { selectAllGroups } from '../store/groups.selectors';
 
 @Component({
   selector: 'cc-group-list',
@@ -7,7 +14,14 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class GroupListComponent implements OnInit {
-  constructor() {}
+  groupsLinks = groupsNavigation;
 
-  ngOnInit(): void {}
+  groupList$: Observable<Group[]> | undefined;
+
+  constructor(private store: Store<AppState>) {}
+
+  ngOnInit(): void {
+    this.store.dispatch(actionGroupsRetrieveAll());
+    this.groupList$ = this.store.pipe(select(selectAllGroups));
+  }
 }
