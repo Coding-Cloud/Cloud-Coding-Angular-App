a2302883f88200eabd7c4ab59afc50fff54ae955
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-04-23 16:50:15 +0200

    feat(cc-65): update group

--- src/app/features/groups/groups.module.ts
+++ src/app/features/groups/groups.module.ts
@@ -5,9 +5,15 @@ import { SharedModule } from '../../shared/shared.module';
 import { GroupsRoutingModule } from './groups-routing.module';
 import { GroupAddComponent } from './group-add/group-add.component';
 import { GroupViewComponent } from './group-view/group-view.component';
+import { GroupEditComponent } from './group-edit/group-edit.component';
 
 @NgModule({
-  declarations: [GroupListComponent, GroupAddComponent, GroupViewComponent],
+  declarations: [
+    GroupListComponent,
+    GroupAddComponent,
+    GroupViewComponent,
+    GroupEditComponent
+  ],
   imports: [CommonModule, SharedModule, GroupsRoutingModule]
 })
 export class GroupsModule {}
