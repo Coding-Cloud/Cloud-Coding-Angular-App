0d6a7fb40c3245b9182c0fac2fc5e363f07f7f25
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-01 15:49:43 +0200

    feat(cc-127): link all user links

--- src/app/features/groups/group-view/group-view.component.ts
+++ src/app/features/groups/group-view/group-view.component.ts
@@ -31,6 +31,7 @@ import { ProjectSearchDialogComponent } from '../../projects/project-search-dial
 import { UserSearchDialogComponent } from '../../users/user-search-dialog/user-search-dialog.component';
 import { Project } from '../../../shared/models/project.model';
 import { map } from 'rxjs/operators';
+import { usersNavigation } from '../../users/users-routing.module';
 
 @Component({
   selector: 'cc-group-view',
@@ -40,16 +41,19 @@ import { map } from 'rxjs/operators';
 })
 export class GroupViewComponent implements OnInit {
   groupId = '';
+  currentUserId = '';
+  groupOwnerId = '';
   group$: Observable<Group>;
   members$: Observable<GroupMembership[]>;
   editMode$: Observable<boolean>;
   currentUser$: Observable<User>;
-  currentUserId = '';
   messages$: Observable<Message[]>;
 
   projectsLinks = projectsNavigation;
   rootLinks = navigation;
   projectViewLink = `/${this.rootLinks.projets.path}/${this.projectsLinks.viewProject.path}`;
+  userLinks = usersNavigation;
+  userViewLink = `/${this.rootLinks.users.path}/${this.userLinks.viewUser.path}`;
 
   constructor(
     private route: ActivatedRoute,
@@ -57,6 +61,9 @@ export class GroupViewComponent implements OnInit {
     private dialog: MatDialog
   ) {
     this.group$ = this.store.pipe(select(selectCurrentGroup));
+    this.group$.subscribe((group) => {
+      this.groupOwnerId = group.ownerId;
+    });
     this.members$ = this.store.pipe(select(selectCurrentGroupMembers));
     this.editMode$ = this.store.pipe(select(selectCurrentGroupIsEditMode));
     this.messages$ = this.store.pipe(select(selectCurrentGroupMessages));
