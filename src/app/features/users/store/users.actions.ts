6009a0107239e00100d68d95f10be86c1fac6d1a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-01 14:40:38 +0200

    feat(cc-130): create actions

--- src/app/features/users/store/users.actions.ts
+++ src/app/features/users/store/users.actions.ts
@@ -1,5 +1,6 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from '../../../shared/models/user.model';
+import { Project } from '../../../shared/models/project.model';
 
 export const actionUsersSearchInit = createAction('[Users] Search Init');
 
@@ -15,5 +16,35 @@ export const actionUsersSearchSuccess = createAction(
 
 export const actionUsersSearchError = createAction(
   '[Users] Search users error',
+  props<{ message: string }>()
+);
+
+export const actionUsersGetOne = createAction(
+  '[Users] Get One',
+  props<{ id: string }>()
+);
+
+export const actionUsersGetOneSuccess = createAction(
+  '[Users] Get One Success',
+  props<{ user: User }>()
+);
+
+export const actionUsersGetOneError = createAction(
+  '[Users] Get One Error',
+  props<{ message: string }>()
+);
+
+export const actionUsersGetUserProjects = createAction(
+  "[Users] Get user's projects",
+  props<{ id: string }>()
+);
+
+export const actionUsersGetUserProjectsSuccess = createAction(
+  "[Users] Get user's projects Success",
+  props<{ projects: Project[] }>()
+);
+
+export const actionUsersGetUserProjectsError = createAction(
+  "[Users] Get user's projects Error",
   props<{ message: string }>()
 );
