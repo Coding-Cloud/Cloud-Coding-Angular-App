829c1e280650c696fbf191ae0f58dc511ca2a611
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-26 16:49:44 +0200

    feat(comment update): start to implement comment edit + delete

--- src/app/features/social/comments/comment-view/comment-view.component.ts
+++ src/app/features/social/comments/comment-view/comment-view.component.ts
@@ -12,6 +12,15 @@ import schema from '../schema';
 import { Comment } from '../../../../shared/models/comment.model';
 import { userViewLink } from '../../users/users-routing.module';
 import { projectViewLink } from '../../../projects/projects-routing.module';
+import { select, Store } from '@ngrx/store';
+import { selectUser } from '../../../../core/auth/auth.selectors';
+import { AppState } from '../../../../core/core.state';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../../../shared/confirm-dialog/confirm-dialog.component';
+import {
+  actionCommentsDeleteOne,
+  actionCommentsUpdateOne
+} from '../store/comments.actions';
 
 @Component({
   selector: 'cc-comment-view',
@@ -31,6 +40,9 @@ export class CommentViewComponent implements OnInit, OnDestroy {
     ownerId: ''
   };
 
+  currentUserId = '';
+  editMode = false;
+
   userViewLink = userViewLink;
   projectViewLink = projectViewLink;
 
@@ -49,6 +61,12 @@ export class CommentViewComponent implements OnInit, OnDestroy {
     })
   });
 
+  constructor(private store: Store<AppState>, private dialog: MatDialog) {
+    this.store
+      .pipe(select(selectUser))
+      .subscribe((user) => (this.currentUserId = user.id));
+  }
+
   ngOnInit(): void {
     this.commentFormGroup.controls.commentContentView.setValue(
       JSON.parse(this.comment.content)
@@ -57,5 +75,38 @@ export class CommentViewComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.editorView.destroy();
+  }
+
+  onEditSwitch() {
+    this.editMode = !this.editMode;
+  }
+
+  onUpdateSubmit(newContent: string) {
+    this.commentFormGroup.controls.commentContentView.setValue(
+      JSON.parse(newContent)
+    );
+    this.store.dispatch(
+      actionCommentsUpdateOne({
+        comment: {
+          id: this.comment.id,
+          content: newContent
+        }
+      })
+    );
+    this.editMode = false;
+  }
+
+  onDelete() {
+    const confirmDialog = this.dialog.open(ConfirmDialogComponent, {
+      data: {
+        title: "Suppression d'un commentaire",
+        message: 'Êtes vous sûr de vouloir supprimer ce commentaire ?'
+      }
+    });
+    confirmDialog.afterClosed().subscribe((result) => {
+      if (result === true) {
+        this.store.dispatch(actionCommentsDeleteOne({ id: this.comment.id }));
+      }
+    });
   }
 }
