846311c982c925bda44b2c91abe5f1c6e1cbae10
Rémy Machavoine <remygt@hotmail.fr>
2022-05-27 23:38:23 +0200

    feat(iframe): add first example iframe CC-161

--- src/app/features/code-editor-v2/components/code-editor/components/front-view/front-view.component.ts
+++ src/app/features/code-editor-v2/components/code-editor/components/front-view/front-view.component.ts
@@ -4,7 +4,8 @@ import {
   ChangeDetectionStrategy,
   Input,
   ViewChild,
-  ElementRef
+  ElementRef,
+  ChangeDetectorRef
 } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { CheckHealthPathService } from '../../../../services/check-health-path.service';
@@ -37,13 +38,9 @@ export class FrontViewComponent implements OnInit {
   constructor(
     private sanitizer: DomSanitizer,
     private checkHealthPathService: CheckHealthPathService,
-    private notificationService: NotificationService
-  ) {
-    //const domain = parseDomain('localhost').hostname;
-    //if (domain) {
-    //  document.domain = domain.toString();
-    //}
-  }
+    private notificationService: NotificationService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     if (this.url !== undefined) {
@@ -70,30 +67,77 @@ export class FrontViewComponent implements OnInit {
   handleChange(event: any) {
     if (event.key === 'Enter' && this.projectUniqueName) {
       this.loadingRequest$.next(true);
-      this.checkHealthPathService
-        .checkUrlIsReachable(this.projectUniqueName, event.target.value)
-        .pipe(finalize(() => this.loadingRequest$.next(false)))
-        .subscribe((isReachable: boolean) => {
-          if (isReachable) {
-            this.urlSee = this.formatUrl(event.target.value);
-            this.baseUrlPathTrust =
-              this.sanitizer.bypassSecurityTrustResourceUrl(event.target.value);
-            this.inputUrl?.nativeElement.blur();
-          } else {
-            this.notificationService.warn(
-              `l\' url ${event.target.value} n'existe pas`
-            );
-            this.inputUrl?.nativeElement.blur();
-          }
-        });
+      setTimeout(() => {
+        this.loadingRequest$.next(false);
+        this.baseUrlPathTrust = this.sanitizer.bypassSecurityTrustResourceUrl(
+          event.target.value
+        );
+        this.inputUrl?.nativeElement.blur();
+      }, 2000);
+      //this.checkHealthPathService
+      //  .checkUrlIsReachable(this.projectUniqueName, event.target.value)
+      //  .pipe(finalize(() => this.loadingRequest$.next(false)))
+      //  .subscribe((isReachable: boolean) => {
+      //    if (isReachable) {
+      //      this.urlSee = this.formatUrl(event.target.value);
+      //      this.baseUrlPathTrust =
+      //        this.sanitizer.bypassSecurityTrustResourceUrl(event.target.value);
+      //      this.inputUrl?.nativeElement.blur();
+      //    } else {
+      //      this.notificationService.warn(
+      //        `l\' url ${event.target.value} n'existe pas`
+      //      );
+      //      this.inputUrl?.nativeElement.blur();
+      //    }
+      //  });
     }
   }
 
   handleLoad($event: any) {
     const location = this.iframeElement?.nativeElement.contentWindow?.location;
+    console.log(location);
+    if (location !== undefined) {
+      console.log(this.iframeElement?.nativeElement.contentWindow);
+      this.iframeElement?.nativeElement.contentWindow?.postMessage(
+        'Request DOM manipulation',
+        'http://localhost:8000'
+      );
+
+      window.addEventListener('message', (event) => {
+        if (event.origin !== 'http://localhost:8000') return;
+        console.log('data');
+        const urlFormat = this.formatUrl(event.data);
+        const urlSeeFormat = this.formatUrl(this.urlSee);
+        if (urlSeeFormat !== urlFormat) {
+          console.log(urlFormat);
+          this.urlSee = urlFormat;
+          this.cd.markForCheck();
+        }
+      });
+      /*this.iframeElement?.nativeElement.contentWindow?.addEventListener(
+        'message',
+        (event) => {
+          console.log(event.origin)
+          // Reject all messages except ones from https://video.example.com
+          if (event.origin !== 'http://localhost:8000') return;
+
+          // Filter success messages
+          console.log(event.data)
+          if (event.data === 'succeeded') {
+            console.log('dans le succeed');
+            console.log(event.data);
+            // DOM manipulation is succeeded
+          }
+        }
+      );*/
+    }
   }
 
   private formatUrl(url: string | undefined): string | undefined {
-    return url?.replace('http://', '').replace('https://', '');
+    let urlReformat = url;
+    if (url?.endsWith('/')) {
+      urlReformat = url.substring(0, url.length - 1);
+    }
+    return urlReformat?.replace('http://', '').replace('https://', '');
   }
 }
