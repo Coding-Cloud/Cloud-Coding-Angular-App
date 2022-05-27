786d20169aef6040a4c698d67aba4d49f03c6e20
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-27 18:48:24 +0200

    feat(user comments): added comment list on user view

--- src/app/features/social/users/user-view/user-view.component.ts
+++ src/app/features/social/users/user-view/user-view.component.ts
@@ -16,6 +16,16 @@ import {
 import { projectsNavigation } from '../../../projects/projects-routing.module';
 import { navigation } from '../../../../app-routing.module';
 import { usersNavigation } from '../users-routing.module';
+import { Comment } from '../../../../shared/models/comment.model';
+import {
+  selectAllComments,
+  selectCommentsLoading
+} from '../../comments/store/comments.selectors';
+import { selectUser } from '../../../../core/auth/auth.selectors';
+import {
+  actionCommentsGetFromUser,
+  actionCommentsInit
+} from '../../comments/store/comments.actions';
 
 @Component({
   selector: 'cc-user-view',
@@ -27,6 +37,9 @@ export class UserViewComponent implements OnInit {
   userId = '';
   user$: Observable<User>;
   projects$: Observable<Project[]>;
+  currentUser$: Observable<User>;
+  userComments$: Observable<Comment[]>;
+  userCommentsLoading$: Observable<boolean>;
 
   usersNavigation = usersNavigation;
   projectsLinks = projectsNavigation;
@@ -36,11 +49,17 @@ export class UserViewComponent implements OnInit {
   constructor(private route: ActivatedRoute, private store: Store<AppState>) {
     this.user$ = this.store.pipe(select(selectUserView));
     this.projects$ = this.store.pipe(select(selectUserViewProjects));
+
+    this.userComments$ = this.store.pipe(select(selectAllComments));
+    this.userCommentsLoading$ = this.store.pipe(select(selectCommentsLoading));
+    this.currentUser$ = this.store.pipe(select(selectUser));
   }
 
   ngOnInit(): void {
     this.userId = this.route.snapshot.paramMap.get('id') ?? '';
     this.store.dispatch(actionUsersGetOne({ id: this.userId }));
     this.store.dispatch(actionUsersGetUserProjects({ id: this.userId }));
+    this.store.dispatch(actionCommentsInit());
+    this.store.dispatch(actionCommentsGetFromUser({ userId: this.userId }));
   }
 }
