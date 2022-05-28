246cb42ed8619d51265be75b38134c0fcd079634
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-05-28 22:20:42 +0200

    feat(clean): remove unused comments

--- src/app/features/social/comments/comments.service.ts
+++ src/app/features/social/comments/comments.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { API_RESOURCE_URI } from '../../../shared/api-resource-uri/api-resource-uri';
 import {
   Comment,
   CreateComment,
   UpdateComment
 } from '../../../shared/models/comment.model';
-import { v4 as uuidv4 } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -26,52 +25,6 @@ export class CommentsService {
   getUserPublicComments(
     userId: string
   ): Observable<{ comments: Comment[]; totalResults: number }> {
-    /* return of({
-      comments: [
-        {
-          id: '16541',
-          projectId: '7b0d1dcb-cb28-436c-83f1-6bc718381d7b',
-          ownerId: userId,
-          content: JSON.stringify({
-            type: 'doc',
-            content: [
-              {
-                type: 'codemirror',
-                content: [
-                  {
-                    type: 'text',
-                    text: 'function max(a, b) {\n  return a > b ? a : b\n}'
-                  }
-                ]
-              }
-            ]
-          }),
-          createdAt: new Date()
-        },
-        {
-          id: '1123',
-          projectId: '7b0d1dcb-cb28-436c-83f1-6bc718381d7b',
-          ownerId: userId,
-          content: JSON.stringify({
-            type: 'doc',
-            content: [
-              {
-                type: 'codemirror',
-                content: [
-                  {
-                    type: 'text',
-                    text: 'function max(a, b) {\n  return a > b ? a : b\n}'
-                  }
-                ]
-              }
-            ]
-          }),
-          createdAt: new Date()
-        }
-      ],
-      totalResults: 2
-    }); */
-
     return this.http.get<{ comments: Comment[]; totalResults: number }>(
       API_RESOURCE_URI.COMMENTS_USER + '/' + userId
     );
@@ -86,7 +39,6 @@ export class CommentsService {
   }
 
   updateComment(comment: UpdateComment): Observable<void> {
-    // return of();
     return this.http.patch<void>(API_RESOURCE_URI.COMMENTS + '/' + comment.id, {
       ...comment
     });
