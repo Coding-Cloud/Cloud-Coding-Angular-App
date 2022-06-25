8dfe1244069bc70076092592142bdae9885a5bae
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2022-06-25 15:07:11 +0200

    feat(home): add type writter on homepage

--- src/app/features/home/home/home.component.ts
+++ src/app/features/home/home/home.component.ts
@@ -15,5 +15,49 @@ export class HomeComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    const typeWriter = require('t-writer.js');
+    const target = document.querySelector('.tw');
+
+    const writer = new typeWriter(target, {
+      loop: true,
+      typeSpeed: 50,
+      deleteSpeed: 30,
+      typeColor: '#69f5ff'
+    });
+
+    writer
+      .type('Coder dans votre langage')
+      .rest(1500)
+      .remove(7)
+      .rest(200)
+      .type('framework')
+      .rest(1500)
+      .clear()
+      .type('Voir vos changements en temps réel')
+      .rest(1500)
+      .remove(13)
+      .rest(200)
+      .type('grâce à une url partageable')
+      .rest(2000)
+      .clear()
+      .type('Partager votre code à vos amis')
+      .rest(1500)
+      .remove(8)
+      .rest(200)
+      .type('votre organisation')
+      .rest(1500)
+      .remove(18)
+      .rest(200)
+      .type('tout le monde')
+      .rest(1500)
+      .clear()
+      .type('Commenter vos projets')
+      .rest(1500)
+      .remove(11)
+      .rest(200)
+      .type('les projets publiques')
+      .rest(5000)
+      .start();
+  }
 }
